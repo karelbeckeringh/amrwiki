471f87c76d44d7fc58756e575b2e53d604420c75
karelbeckeringh <karelbeckeringh@live.nl>
2018-04-25 15:53:32 +0200

    Add files via upload

--- extract.py
+++ extract.py
@@ -3,14 +3,27 @@ import pickle
 
 def main():
 	amrs = {}
+	amr = 0
+	c = 0
 
 	for line in stdin:
 		if line[0:5] == "# AMR":
-			corpus = line.split()[4]
-			section = line.split()[6]
-		elif not line.split():
-			new
+			corpus = line.split()[4][:-1]
+			section = line.split()[6][:-1]
+		elif line[0:6] == "# ::id":
+			ID = line.split()[2]
+			amr = str()
+		elif line.split():
+			if line[0] != "#":
+				amr += line
+		else:
+			if amr:
+				if ":wiki \"" in amr:
+					amrs[ID] = amr
+					c += 1
 
+	#print(c)
+	pickle.dump(amrs, open(str(section)+"-"+str(corpus)+".pickle", "wb"))
 
 if __name__ == '__main__':
 	main()
